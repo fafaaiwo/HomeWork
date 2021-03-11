1cb80abe2dedbbd1ea44cf58235ce64e65c5876a
Galaxy <67579565+fafaaiwo@users.noreply.github.com>
2021-03-11 15:28:54 +0800

    Add files via upload

---
+++ HomeWork3/1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace HomeWork01
+{
+    interface shape
+    {
+        double getMeasure();
+    }
+
+    class triangle : shape
+    {
+        private int a, b, c;//三角形的三边
+
+        public triangle()
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+        }
+
+        public triangle(int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public bool isTriangle()
+        {
+            if (a + b > c && a + c > b && b + c > a && a - b < c && a - c < b && b - c < a)
+            {
+                Console.WriteLine("三角形合法");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("三角形不合法");
+                return false;
+            }
+
+        }
+        public double getMeasure()
+        {
+            double area = 0;
+            if (isTriangle())
+            {
+                double p = (a + b + c) / 2;
+                area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+            return area;
+        }
+    }
+
+    class rectangle : shape
+    {
+        private int a, b;
+
+        public rectangle()
+        {
+            a = 0;
+            b = 0;
+        }
+
+        public rectangle(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public double getMeasure()
+        {
+            return a * b;
+        }
+
+    }
+
+    class square : shape
+    {
+        private int a;
+
+        public square()
+        {
+            a = 0;
+        }
+        public square(int a)
+        {
+            this.a = a;
+        }
+
+
+        public double getMeasure()
+        {
+            return a * a;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            triangle A = new triangle(3, 4, 5);
+            Console.WriteLine(A.isTriangle());
+            Console.WriteLine(A.getMeasure());
+            rectangle B = new rectangle(3, 4);
+            Console.WriteLine(B.getMeasure());
+            square C = new square(3);
+            Console.WriteLine(C.getMeasure());
+
+        }
+    }
+
+}
+
+
---
+++ HomeWork3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HomeWork01
+{
+    public class ShapeFactory
+    {
+        public static shape createShape(string s)
+        {
+            shape sp = null;
+            switch (s){
+                case "三角形":
+                    sp = new triangle();
+                    break;
+                case "矩形":
+                    sp = new rectangle();
+                    break;
+                case "正方形":
+                    sp = new square();
+                    break;
+                default:
+                    throw new Exception("参数错误");                   
+            }
+            return sp;
+        }
+    }
+
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            double totalArea=0;
+            Random random = new Random();
+            string[] a = { "正方形", "矩形", "三角形" };
+            for (int i = 0; i <= 10; i++)
+            {
+                int rand = random.Next(0, 3);
+                shape Sp = ShapeFactory.createShape(a[rand]);
+                Sp.set();
+                totalArea += Sp.getMeasure();
+            }
+            Console.WriteLine(totalArea);
+            
+        }
+    }
+
+}
+
+
